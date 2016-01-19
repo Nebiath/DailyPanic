623b3c338c606e563a5cf8a537ef3d4f9ddca062
Nebiath <nebiath2@hotmail.com>
2016-01-19 10:00:46 +0100

    Update VM_Datastores.ps1

--- VM_Datastores.ps1
+++ VM_Datastores.ps1
@@ -1,3 +1,18 @@
+<#
+.SYNOPSIS
+   Get datastores da
+.DESCRIPTION
+   This PowerCLI script takes input from a list of datastores in the cluster and outputs some data
+   of the VMs inside it. Specifically: VM Name, Powerstate, Provisioned and used size (In GB) 
+  
+  This output is saved to a CSV file.
+#>
+
+## Connect to vCenter
+$vcenter = 'IP_Vcenter'
+Write-Host "Connecting to $vcenter" -ForegroundColor Green
+Connect-VIServer $vcenter | Out-Null
+
 $ds_str = Get-Content C:\temp\Datastores.txt
 
 $dss = Get-Datastore -name $ds_str
