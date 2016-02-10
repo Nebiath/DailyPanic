0f65e886a9f17c7de64dbbe5555460abe655d23d
Nebiath <nebiath2@hotmail.com>
2016-02-10 15:55:50 +0100

    Update and rename Change_VM_Memory to Change_VM_Memory.ps1

--- Change_VM_Memory.ps1
+++ Change_VM_Memory.ps1
@@ -1,3 +1,19 @@
+
+<#
+.SYNOPSIS
+   Change VM Memory in batch
+.DESCRIPTION
+   This PowerCLI script changes the memory of a list of given VMs.
+   It takes the data from a TXT file.
+   VMs are powered down before and after the change.
+   
+#>
+
+## Connect to vCenter
+$vcenter = 'IP_Vcenter'
+Write-Host "Connecting to $vcenter" -ForegroundColor Green
+Connect-VIServer $vcenter | Out-Null
+
 # Get requested VMs
 $vms = Get-VM (Get-Content "C:\temp\VM.txt") 
 
