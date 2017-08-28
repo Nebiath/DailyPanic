dbb642f4250cef6cb8bc5e86399998a2b5de49a0
Nebiath <nebiath2@hotmail.com>
2017-08-28 10:35:56 +0200

    Create Update_NTP.ps1

---
+++ Update_NTP.ps1
@@ -0,0 +1,22 @@
+## Connect to vCenter
+$vcenter = 'IP_Vcenter'
+Write-Host "Connecting to $vcenter" -ForegroundColor Green
+Connect-VIServer $vcenter | Out-Null
+
+$ESXi = Get-Content C:\temp\ESXI.txt
+
+Foreach ($esxname in $ESXi) {
+
+    ## Se elimina el servidor NTP actual
+
+    Remove-VmHostNtpServer -NtpServer 14.10.65.10, 217.10.94.16 -VMHost $esxname –confirm 
+
+    ## Se instroduce la IP del nuevo servidor NTP
+
+    Add-VmHostNtpServer -NtpServer 160.118.44.133, 160.118.44.134 -VMHost $esxname
+
+    ## Se produce el reinicio de servicio NTP
+
+    Get-VMHostService $esxname | where { $_.Key -eq “ntpd” } | Restart-VMHostService
+
+}
