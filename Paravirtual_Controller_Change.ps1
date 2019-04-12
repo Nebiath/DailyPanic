1460ad02ddd7dabe61b38ebad39222b037858759
Urgel, Carlos {FIRE~Madrid-Josefa Valcarcel} <carlos.urgel@contractors.roche.com>
2019-04-12 14:33:08 +0200

    Added foreach statement for batch processing the VM list and adjusted sleeping process.

--- Paravirtual_Controller_Change.ps1
+++ Paravirtual_Controller_Change.ps1
@@ -33,61 +33,64 @@ $WarningPreference = 'SilentlyContinue'
 
 # Defined variables
 # VM list to be modified. One name per line. It's a good idea to separate the VMs by vcenter.
-$vmName = Get-Content "C:\Path_To_File\VMs.txt"
+$vmName = Get-Content "C:\Webs\VMs.txt"
 
-Start-Transcript -Path "C:\Path_To_File\Transcript.txt" -Append
+Start-Transcript -Path "C:\Webs\Transcript.txt" -Append
 
-Write-Host (get-date -uformat %I:%M:%S) "Processing virtual machine:" $vmName -ForegroundColor Green
+foreach($VM in $vmName) {
 
-# VM Shutdown
-Write-Host `t(get-date -uformat %I:%M:%S) "Shutting down virtual machine" -ForegroundColor Green
-shutdown-vmguest -vm $vmName -Confirm:$false | Out-Null
-do {start-sleep -s 3;$powerstate = get-vm $vmName | % {$_.PowerState}} while ($powerstate -ne "PoweredOff")
+  Write-Host (get-date -uformat %I:%M:%S) "Processing virtual machine:" $VM -ForegroundColor Green
 
-# Add the new disk on Paravirtual Adapter Type
-Write-Host `t(get-date -uformat %I:%M:%S) "Adding temporary hard drive on paravirtual SCSI controller" -ForegroundColor Green
-Get-VM $vmName | New-HardDisk -CapacityKB 1048 -StorageFormat Thin | New-ScsiController -Type paravirtual | Out-Null
+  # VM Shutdown
+  Write-Host `t(get-date -uformat %I:%M:%S) "Shutting down virtual machine" -ForegroundColor Green
+  shutdown-vmguest -vm $VM -Confirm:$false | Out-Null
+  do {start-sleep -s 3;$powerstate = get-vm $VM | % {$_.PowerState}} while ($powerstate -ne "PoweredOff")
 
-# Waiting a bit to start the VM because reasons...
-Start-Sleep -s 10
+  # Add the new disk on Paravirtual Adapter Type
+  Write-Host `t(get-date -uformat %I:%M:%S) "Adding temporary hard drive on paravirtual SCSI controller" -ForegroundColor Green
+  Get-VM $VM | New-HardDisk -CapacityKB 1048 -StorageFormat Thin | New-ScsiController -Type paravirtual | Out-Null
 
-# VM start and wait a bit for the drivers to be installed
-Write-Host `t(get-date -uformat %I:%M:%S) "Starting virtual machine..." -ForegroundColor Green
-start-vm -vm $vmName -Confirm:$false | Out-Null
-do {start-sleep -s 3;$vmwaretools = get-vm $vmName | % {get-view $_.ID} | % {$_.Guest.ToolsRunningStatus}} while ($vmwaretools -eq "guestToolsNotRunning")
+  # Waiting a bit to start the VM because reasons...
+  Start-Sleep -s 10
 
-Start-Sleep -s 20
+  # VM start and wait a bit for the drivers to be installed
+  Write-Host `t(get-date -uformat %I:%M:%S) "Starting virtual machine..." -ForegroundColor Green
+  start-vm -vm $VM -Confirm:$false | Out-Null
+  do {start-sleep -s 3;$vmwaretools = get-vm $VM | % {get-view $_.ID} | % {$_.Guest.ToolsRunningStatus}} while ($vmwaretools -eq "guestToolsNotRunning")
 
-# Second VM Shutdown
-Write-Host `t(get-date -uformat %I:%M:%S) "Shutting down virtual machine..." -ForegroundColor Green
-shutdown-vmguest -vm $vmName -Confirm:$false | Out-Null
-do {start-sleep -s 3;$powerstate = get-vm $vmName | % {$_.PowerState}} while ($powerstate -ne "PoweredOff")
+  Start-Sleep -s 10
 
-# VM disk reconfiguration
-Write-Host `t(get-date -uformat %I:%M:%S) "Removing temporary hard drive from virtual machine" -ForegroundColor Green
-Get-HardDisk -vm $vmName | Where {$_.CapacityKB -eq 1048} | Remove-HardDisk -DeletePermanently -Confirm:$False | Out-Null
-Write-Host `t(get-date -uformat %I:%M:%S) "Changing Primary SCSI controller to paravirtual" -ForegroundColor Green
-Get-HardDisk -VM $vmName | Select -First 1 | Get-ScsiController | Set-ScsiController -Type paravirtual | Out-Null
+  # Second VM Shutdown
+  Write-Host `t(get-date -uformat %I:%M:%S) "Shutting down virtual machine..." -ForegroundColor Green
+  shutdown-vmguest -vm $VM -Confirm:$false | Out-Null
+  do {start-sleep -s 3;$powerstate = get-vm $VM | % {$_.PowerState}} while ($powerstate -ne "PoweredOff")
 
-# Waiting a bit to start the VM because reasons, again...
-Start-Sleep -s 10
-Write-Host `t(get-date -uformat %I:%M:%S) "Starting virtual machine" -ForegroundColor Green
-start-vm -vm $vmName -Confirm:$false | Out-Null
+  # VM disk reconfiguration
+  Write-Host `t(get-date -uformat %I:%M:%S) "Removing temporary hard drive from virtual machine" -ForegroundColor Green
+  Get-HardDisk -vm $VM | Where {$_.CapacityKB -eq 1048} | Remove-HardDisk -DeletePermanently -Confirm:$False | Out-Null
+  Write-Host `t(get-date -uformat %I:%M:%S) "Changing Primary SCSI controller to paravirtual" -ForegroundColor Green
+  Get-HardDisk -vm $VM | Select -First 1 | Get-ScsiController | Set-ScsiController -Type paravirtual | Out-Null
 
-# Changing the advanced setting disk.enableUUID
-Write-Host `t(get-date -uformat %I:%M:%S) "Checking if disk.enableUUID is set to TRUE" -ForegroundColor Green
-if((get-vm $vmName | Get-AdvancedSetting -Name disk.enableUUID).Value -ne "true"){
-	Get-VM $vmName | New-AdvancedSetting -Name "disk.enableUUID" -Value "true" -Confirm:$false | Out-Null
-	Write-Host `t(get-date -uformat %I:%M:%S) "Changing disk.enableUUID to TRUE" -ForegroundColor Green
+  # Waiting a bit to start the VM because reasons, again...
+  Start-Sleep -s 10
+  Write-Host `t(get-date -uformat %I:%M:%S) "Starting virtual machine" -ForegroundColor Green
+  start-vm -vm $VM -Confirm:$false | Out-Null
+
+  # Changing the advanced setting disk.enableUUID
+  Write-Host `t(get-date -uformat %I:%M:%S) "Checking if disk.enableUUID is set to TRUE" -ForegroundColor Green
+  if((get-vm $VM | Get-AdvancedSetting -Name disk.enableUUID).Value -ne "true"){
+	   Get-VM $VM | New-AdvancedSetting -Name "disk.enableUUID" -Value "true" -Confirm:$false | Out-Null
+	    Write-Host `t(get-date -uformat %I:%M:%S) "Changing disk.enableUUID to TRUE" -ForegroundColor Green
+    }
+    else {
+	     Write-Host `t(get-date -uformat %I:%M:%S) "Disk.enableUUID already set to TRUE - Nothing to do" -ForegroundColor Green
+     }
+
+     Start-Sleep -s 10
+
+     # Online disks with PVSCSI resolving the online policy
+     Write-Host `t(get-date -uformat %I:%M:%S) "Bringing the disks online" -ForegroundColor Green
+     Invoke-VMScript -vm $VM -GuestUser $User -GuestPassword $Password -ScriptText 'get-disk | where OperationalStatus -eq "Offline" | %{$_.Number ; Set-Disk -Number $_.Number -IsOffline $false; Set-Disk -Number $_.Number -IsReadOnly $false}' -Verbose:$false | Write-Verbose
+
+     Write-Host (get-date -uformat %I:%M:%S) "Processing completed! Please review change log above." -ForegroundColor Green
 }
-else {
-	Write-Host `t(get-date -uformat %I:%M:%S) "Disk.enableUUID already set to TRUE - Nothing to do" -ForegroundColor Green
-}
-
-Start-Sleep -s 40
-
-# Online disks with PVSCSI resolving the online policy
-Write-Host `t(get-date -uformat %I:%M:%S) "Bringing the disks online" -ForegroundColor Green
-Invoke-VMScript -VM $vmName -GuestUser $User -GuestPassword $Password -ScriptText 'get-disk | where OperationalStatus -eq "Offline" | %{$_.Number ; Set-Disk -Number $_.Number -IsOffline $false; Set-Disk -Number $_.Number -IsReadOnly $false}' -Verbose:$false | Write-Verbose
-
-Write-Host (get-date -uformat %I:%M:%S) "Processing completed! Please review change log above." -ForegroundColor Green
