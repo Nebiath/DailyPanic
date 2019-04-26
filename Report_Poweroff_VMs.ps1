a26b356433b9174391273ec1c7412d0eee1b6dab
Urgel, Carlos {FIRE~Madrid-Josefa Valcarcel} <carlos.urgel@contractors.roche.com>
2019-04-26 13:50:52 +0200

    Initial commit.

---
+++ Report_Poweroff_VMs.ps1
@@ -0,0 +1,53 @@
+<# VM_ParavirtualSCSI_Config.ps1
+.Synopsis
+
+.DESCRIPTION
+   PowerShell script to
+.EXAMPLE
+
+.INPUTS
+
+.NOTES
+
+.FUNCTIONALITY
+
+#>
+
+$Global:Report = @()
+
+
+If($? -Eq $True)
+
+{
+
+	$PoweredOffAge = (Get-Date).AddDays(-30)
+	$Output = @{}
+	$PoweredOffvms = Get-VM | where {$_.PowerState -eq "PoweredOff"}
+	$EventsLog = Get-VIEvent -Entity $PoweredOffvms -Finish $PoweredOffAge  -MaxSamples ([int]::MaxValue) | where{$_.FullFormattedMessage -like "*is powered off"}
+	If($EventsLog)
+	{
+		$EventsLog | %{ if($Output[$_.Vm.Name] -lt $_.CreatedTime)
+			{
+				$Output[$_.Vm.Name] = $_.CreatedTime
+			}
+		}
+	}
+	$Result = $Output.getEnumerator() | select @{N="VM";E={$_.Key}},@{N="Powered Off Date";E={$_.Value}}
+
+	If($Result)
+	{
+		$Result | Export-Csv -NoTypeInformation $LogFile
+	}
+	Else
+	{
+		"NO VM's Powered off last 30 Days"
+	}
+}
+Else
+{
+	Write-Host "Error in Connecting to $VIServer; Try Again with correct user name & password!" -Foregroundcolor "Red"
+}
+
+Disconnect-VIServer * -Confirm:$false
+#
+#-------------------------------------------------------------------------------------------------------------
